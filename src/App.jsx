b3e45bf4ba9ad33e4f8b1325e31679b1c9d157fa
Kelvin Huanca <kelvin.elpregonero@gmail.com>
2025-01-21 18:55:34 -0500

    version prueba

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 /* import './App.css' */
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './componentes/NavBar';
 import Home from './pages/Home'
 import Portafolio from './pages/Portafolio'
